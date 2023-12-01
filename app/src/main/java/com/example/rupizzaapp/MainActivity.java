1b6eddca24e8951b76a65d5c637b6f18be6dd99b
deshnadoshi <deshna.doshi@gmail.com>
2023-12-01 13:10:12 -0500

    GUI: Navigating to BYO works

--- app/src/main/java/com/example/rupizzaapp/MainActivity.java
+++ app/src/main/java/com/example/rupizzaapp/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.rupizzaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button byoMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        openBYO(savedInstanceState);
+
     }
+
+    protected void openBYO(Bundle savedInstanceState){
+        byoMain = findViewById(R.id.byoMain);
+        byoMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, BuildYourOwnActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+
+
+
 }
