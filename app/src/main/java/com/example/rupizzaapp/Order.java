263a005a29e52e8961cc334d244439cf6c3851a2
deshnadoshi <deshna.doshi@gmail.com>
2023-12-01 10:56:02 -0500

    Added RUPizza files to directory

---
+++ app/src/main/java/com/example/rupizzaapp/Order.java
@@ -0,0 +1,115 @@
+package com.example.rupizzaapp;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.util.ArrayList;
+
+/**
+ * Creates a Pizza Order.
+ * @author Haejin Song, Deshna Doshi
+ */
+public class Order {
+    private int orderNumber;
+    private static Order orderObject;
+    private ArrayList <Pizza> all_orders;
+
+    /**
+     * Constructor for a Pizza Order
+     */
+    private Order(){
+        StoreOrders storeOrders = StoreOrders.getInstance();
+        orderNumber = storeOrders.getNextOrderNumber();
+        all_orders = new ArrayList<Pizza>();
+    }
+
+    /**
+     * Retrieves the instance of an Order.
+     * @return an Order.
+     */
+    public static Order getInstance() {
+        if (orderObject == null) {
+            orderObject = new Order();
+        }
+        return orderObject;
+    }
+
+    /**
+     * Add a pizza to an order.
+     * @param new_order The Pizza object to be added to an order.
+     */
+    public void addPizza(Pizza new_order){
+        all_orders.add(new_order);
+    }
+
+    /**
+     * Remove a pizza from an order.
+     * @param index The index of the pizza to be removed.
+     */
+    public void removePizza(int index){
+        all_orders.remove(index);
+    }
+
+    /**
+     * Returns the order number of the pizza.
+     * @return a String representing the order number.
+     */
+    public String getOrderNumber(){
+        return String.valueOf(orderNumber);
+    }
+
+    /**
+     * Find a pizza based on the type of pizza.
+     * @param pizza a String describing the pizza.
+     * @return -1 if not found, the index if found.
+     */
+    public int findPizza(String pizza) {
+        for (int i = 0; i < all_orders.size(); i++) {
+            if (pizza.equals(all_orders.get(i).toString())) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Provides all of the orders in a pizza.
+     * @return an ArrayList of Pizzas.
+     */
+    public ArrayList<Pizza> getAllOrders() {
+        return all_orders;
+    }
+
+    /**
+     * Delete an order.
+     */
+    public void deleteOrder() {
+        orderObject = null;
+    }
+
+
+    /**
+     * Creates an ObservableList with all of the pizzas in an order.
+     * @return an ObservableList of <String> with pizza information.
+     */
+    public ObservableList<String> toStringArray() {
+        ObservableList<String> stringList = FXCollections.observableArrayList();
+        for (int i = 0; i < this.all_orders.size(); i++) {
+            stringList.add(String.valueOf(this.all_orders.get(i)));
+        }
+        return stringList;
+    }
+
+    /**
+     * Prints all of the orders.
+     * @return a String that represents all of the orders.
+     */
+    public String allOrdersToString(){
+        String retString = "";
+        for (int i = 0; i < all_orders.size(); i++){
+            retString += (all_orders.get(i).toString() + "\n") ;
+        }
+
+        return retString;
+    }
+
+}
---
+++ app/src/main/java/com/example/rupizzaapp/Topping.java
@@ -0,0 +1,40 @@
+package com.example.rupizzaapp;
+
+
+/**
+ * Enum class to represent the Toppings.
+ * @author Haejin Song, Deshna Doshi
+ */
+public enum Topping {
+    GREENPEPPER(0),
+    ONION(1),
+    PINEAPPLE(2),
+    BLACKOLIVES(3),
+    MUSHROOM(4),
+    SAUSAGE(5),
+    CHICKEN(6),
+    BEEF(7),
+    HAM(8),
+    CRABMEATS(9),
+    PEPPERONI(10),
+    SQUID(11),
+    SHRIMP(12);
+
+    private final int CODE;
+
+    /**
+     * Constructor for the Topping enum.
+     * @param code Integer corresponding to the toppings.
+     */
+    Topping(int code) {
+        this.CODE = code;
+    }
+
+    /**
+     * Provides the code corresponding to the topping.
+     * @return Code (integer) of the topping.
+     */
+    public int getCode() {
+        return CODE;
+    }
+}
