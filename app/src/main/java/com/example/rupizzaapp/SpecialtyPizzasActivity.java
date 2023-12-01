2b3dc7b7120e74b77dbbfbd26db487cdbe1e3299
deshnadoshi <deshna.doshi@gmail.com>
2023-12-01 13:33:13 -0500

    GUI: Navigating to all tabs works

---
+++ app/src/main/java/com/example/rupizzaapp/SpecialtyPizzasActivity.java
@@ -0,0 +1,14 @@
+package com.example.rupizzaapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SpecialtyPizzasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_specialty);
+    }
+}
